08c749941f61b489fc07b68f02dcfec43d407ab1
nicolas <nmalonsojuan@gmail.com>
2022-05-29 09:46:13 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (request, response) => {
+    response.send({ info: 'Node, Express, and Postgres API',
+        endpoints: {
+        '/users': ['get', 'post'],
+        '/users/id': ['get', 'put', 'delete']
+    } })
+});
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
